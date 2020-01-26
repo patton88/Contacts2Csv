954c8d01f51557f92e0264543adf38e138581139
name2017 <name2017@163.com>
2020-01-26 23:02:58 +0800

    Commit015 : 规范变量命名

--- app/src/main/java/com/example/contacts2csv/ContactToolInsertUtils.java
+++ app/src/main/java/com/example/contacts2csv/ContactToolInsertUtils.java
@@ -16,226 +16,223 @@ import android.provider.ContactsContract.Contacts.Data;
 import android.provider.ContactsContract.RawContacts;
 import android.util.Log;
 
-import com.example.contacts2csv.ContactContant;
-import com.example.contacts2csv.ContactInfo;
-import com.example.contacts2csv.ContactToolUtils;
-
 public class ContactToolInsertUtils extends ContactToolUtils {
-	private static final String TAG = "ContactToolInsertUtils";
-	
-	private static int successCount = 0;
-	private static int failCount = 0;
-	private static boolean isGbk = false;
-	
-	public static boolean insertIntoContact(Context context,String path,String charset){
-		init(charset);
-		try{
-			ArrayList<ContactInfo> arrayList = readFromFile(path);
-			if(arrayList == null){
-				Log.e(TAG, "Error in insertIntoContact arrayList == null");
-				return false;
-			}
-			Iterator<ContactInfo> iterator = arrayList.iterator();
-			while(iterator.hasNext()){
-				ContactInfo contactInfo = iterator.next();
-				if(doInsertIntoContact(context,contactInfo)){
-					successCount++;
-				}
-			}
-		}catch (Exception e) {
-			Log.e(TAG, "Error in insertIntoContact result : " + e.getMessage());
-		}
-		return true;
-	}
+    private static final String m_sTAG = "ContactToolInsertUtils";
 
-	private static void init(String charset){
-		successCount = 0;
-		failCount =0;
-		isGbk = charset.equals(ContactContant.CHARSET_GBK);
-	}
-	
-	/**read txt file*/
-	private static ArrayList<ContactInfo> readFromFile(String path){
-		//read from file
-		ArrayList<String> stringsArrayList = doReadFile(path);
-		if(stringsArrayList == null){
-			Log.e(TAG, "Error in readFromFile stringsArrayList == null");
-			return null;
-		}
-		ArrayList<ContactInfo> contactInfoArrayList = handleReadStrings(stringsArrayList);
-		return contactInfoArrayList;
-	}
-	
-	private static ArrayList<String> doReadFile(String path){
-		FileInputStream in = null;	
-		ArrayList<String> arrayList = new ArrayList<String>();
-	    try {
-            byte[] tempbytes = new byte[ContactContant.BUFFER_SIZE];
-            in = new FileInputStream(path);
-            while (in.read(tempbytes) != -1) {
-            	int length = 0;
-            	int first = length;
-            	for(int i = 0;i < tempbytes.length;i++){           		
-            		if(tempbytes[i] == ContactContant.ENTER_CHAR_LINUX){
-            			length = i;
-            			byte[] nowBytes = new byte[length - first];
-                    	System.arraycopy(tempbytes, first, nowBytes, 0, length - first);
-                    	if(isGbk){
-                    		arrayList.add(new String(nowBytes,ContactContant.CHARSET_GBK).trim());
-                    	}
-                    	else {
-                    		arrayList.add(new String(nowBytes,ContactContant.CHARSET_UTF8).trim());
-						}
-                    	first = i + 1;
-            		}
-            	}
-            	
+    private static int m_iSuccessCount = 0;
+    private static int m_iFailCount = 0;
+    private static boolean m_bGbk = false;
+
+    public static boolean insertIntoContact(Context context, String path, String charset) {
+        init(charset);
+        try {
+            ArrayList<ContactInfo> arrayList = readFromFile(path);
+            if (arrayList == null) {
+                Log.e(m_sTAG, "Error in insertIntoContact arrayList == null");
+                return false;
+            }
+            Iterator<ContactInfo> iterator = arrayList.iterator();
+            while (iterator.hasNext()) {
+                ContactInfo contactInfo = iterator.next();
+                if (doInsertToContact(context, contactInfo)) {
+                    m_iSuccessCount++;
+                }
+            }
+        } catch (Exception e) {
+            Log.e(m_sTAG, "Error in insertIntoContact result : " + e.getMessage());
+        }
+        return true;
+    }
+
+    private static void init(String charset) {
+        m_iSuccessCount = 0;
+        m_iFailCount = 0;
+        m_bGbk = charset.equals(ContactContant.CHARSET_GBK);
+    }
+
+    /**
+     * read txt file
+     */
+    private static ArrayList<ContactInfo> readFromFile(String sPath) {
+        //read from file
+        ArrayList<String> sArrList = doReadFile(sPath);
+        if (sArrList == null) {
+            Log.e(m_sTAG, "Error in readFromFile sArrList == null");
+            return null;
+        }
+        ArrayList<ContactInfo> arrListContactInfo = readArrList(sArrList);
+        return arrListContactInfo;
+    }
+
+    private static ArrayList<String> doReadFile(String sPath) {
+        FileInputStream inStream = null;
+        ArrayList<String> arrList = new ArrayList<String>();
+        try {
+            byte[] byteArr = new byte[ContactContant.BUFFER_SIZE];
+            inStream = new FileInputStream(sPath);
+            while (inStream.read(byteArr) != -1) {
+                int iLen = 0;
+                int iFirst = iLen;
+                for (int i = 0; i < byteArr.length; i++) {
+                    if (byteArr[i] == ContactContant.ENTER_CHAR_LINUX) {
+                        iLen = i;
+                        byte[] nowBytes = new byte[iLen - iFirst];
+                        System.arraycopy(byteArr, iFirst, nowBytes, 0, iLen - iFirst);
+                        if (m_bGbk) {
+                            arrList.add(new String(nowBytes, ContactContant.CHARSET_GBK).trim());
+                        } else {
+                            arrList.add(new String(nowBytes, ContactContant.CHARSET_UTF8).trim());
+                        }
+                        iFirst = i + 1;
+                    }
+                }
+
             }
         } catch (Exception e1) {
             return null;
         } finally {
-            if (in != null) {
+            if (inStream != null) {
                 try {
-                    in.close();
+                    inStream.close();
                 } catch (IOException e1) {
-                	return null;
+                    return null;
                 }
             }
         }
-        return arrayList;
-	}
+        return arrList;
+    }
 
-	private static ArrayList<ContactInfo> handleReadStrings(ArrayList<String> arrayList){
-		ArrayList<ContactInfo> contactInfoArrayList = new ArrayList<ContactInfo>();
-		Iterator<String> infos = arrayList.iterator();
-		while(infos.hasNext()){
-			String info = infos.next();
-			String[] infoStrings = info.split(ContactContant.SPACE_REGULAR);
-			String displayName = null;
-			String mobileNum = null;
-			String homeNum = null;
-			switch (infoStrings.length) {
-			case 0:
-				//do nothing
-				continue;
-			case 1:
-				displayName = infoStrings[0];
-				break;
-			case 2:
-				displayName = infoStrings[0];
-				if(infoStrings[1].length() == ContactContant.MOBILE_NUM_LENGTH){
-					mobileNum = infoStrings[1];
-				}
-				else{
-					homeNum = infoStrings[1];
-				}
-				break;
-			default:
-				//length >= 3
-				displayName = infoStrings[0];
-				mobileNum = infoStrings[1];
-				homeNum = infoStrings[2];
-			}
-			//check displayName mobileNum and homeNum
-			if(displayName == null || displayName.matches(ContactContant.NUM_REGULAR)){
-				Log.e(TAG, "Error in handleReadStrings displayName == null");
-				failCount++;
-				continue;
-			}
-			if(mobileNum != null && (mobileNum.length() != ContactContant.MOBILE_NUM_LENGTH 
-					|| !mobileNum.matches(ContactContant.NUM_REGULAR))){
-				Log.e(TAG, "Error in handleReadStrings mobileNum is not all num or mobileNum == null");
-				failCount++;
-				continue;
-			}
-			if(homeNum != null && !(homeNum.matches(ContactContant.HOME_REGULAR_01) || 
-					homeNum.matches(ContactContant.HOME_REGULAR_02))){
-				Log.e(TAG, "Error in handleReadStrings homeNum is not all num");
-				failCount++;
-				continue;
-			}
-			contactInfoArrayList.add(new ContactInfo(displayName, mobileNum, homeNum));
-		}
-		return contactInfoArrayList;
-	}
-	
-	/**insert into database*/
-	private static boolean doInsertIntoContact(Context context,ContactInfo contactInfo){
-		Log.d(TAG, "in doInsertIntoContact contactInfo = null? " + (contactInfo == null));
-		try {
-			ContentValues contentValues = new ContentValues();
-	    	Uri uri = context.getContentResolver().insert(RawContacts.CONTENT_URI, contentValues);
-	    	long rowId = ContentUris.parseId(uri);
-	    	
-	    	String name = contactInfo.getDisplayName();
-	    	String mobileNum = contactInfo.getMobileNum();
-	    	String homeNum = contactInfo.getHomeNum();
-	    	
-	    	//insert name
-	    	if(name != null){
-	        	contentValues.clear();
-	        	contentValues.put(Data.RAW_CONTACT_ID, rowId);
-	        	contentValues.put(Data.MIMETYPE, StructuredName.CONTENT_ITEM_TYPE);
-	    		int index = name.length() / 2;
-	    		String displayName = name;
-	    		//check name if english name
-	    		String givenName = null;
-	    		String familyName = null;
-	    		if(checkEnglishName(displayName) == false){
-	    			givenName = name.substring(index);
-	    		 	familyName = name.substring(0, index);
-	    		}
-	    		else {
-					givenName = familyName = displayName;
-				}
-	    		contentValues.put(StructuredName.DISPLAY_NAME, displayName);
-	        	contentValues.put(StructuredName.GIVEN_NAME, givenName);
-	        	contentValues.put(StructuredName.FAMILY_NAME, familyName);
-	        	context.getContentResolver().insert(ContactsContract.Data.CONTENT_URI, contentValues);
-	    	}
-	    	
-	    	if(mobileNum != null){
-	    		//insert phone
-		    	contentValues.clear();
-		    	contentValues.put(Data.RAW_CONTACT_ID, rowId);
-		    	contentValues.put(Data.MIMETYPE, Phone.CONTENT_ITEM_TYPE);
-		    	contentValues.put(Phone.NUMBER, mobileNum);
-		    	contentValues.put(Phone.TYPE, Phone.TYPE_MOBILE);
-		    	context.getContentResolver().insert(ContactsContract.Data.CONTENT_URI, contentValues);
-	    	}
-	    	
-	    	if(homeNum != null){
-		    	//insert houseNum
-		    	contentValues.clear();
-		    	contentValues.put(Data.RAW_CONTACT_ID, rowId);
-		    	contentValues.put(Data.MIMETYPE, Phone.CONTENT_ITEM_TYPE);
-		    	contentValues.put(Phone.NUMBER, homeNum);
-		    	contentValues.put(Phone.TYPE, Phone.TYPE_HOME);
-		    	context.getContentResolver().insert(ContactsContract.Data.CONTENT_URI, contentValues);
-	    	}
-		} catch (Exception e) {
-			return false;
-		}
-		return true;   	
-	}
-	
-	private static boolean checkEnglishName(String name){
-		char[] nameChars = name.toCharArray();
-		for(int i = 0;i < nameChars.length;i++){
-			if((nameChars[i] >= 'a' && nameChars[i] <= 'z') ||
-					(nameChars[i] >= 'A' && nameChars[i] <= 'Z')){
-				continue;
-			}
-			return false;
-		}
-		return true;
-	}
-		
-	public static int getSuccessCount() {
-		return successCount;
-	}
+    private static ArrayList<ContactInfo> readArrList(ArrayList<String> arrList) {
+        ArrayList<ContactInfo> arrListContactInfo = new ArrayList<ContactInfo>();
+        Iterator<String> it = arrList.iterator();
+        while (it.hasNext()) {
+            String sInfo = it.next();
+            String[] sArrInfo = sInfo.split(ContactContant.SPACE_REGULAR);
+            String sDisplayName = null;
+            String sMobileNum = null;
+            String sHomeNum = null;
+            switch (sArrInfo.length) {
+                case 0:
+                    //do nothing
+                    continue;
+                case 1:
+                    sDisplayName = sArrInfo[0];
+                    break;
+                case 2:
+                    sDisplayName = sArrInfo[0];
+                    if (sArrInfo[1].length() == ContactContant.MOBILE_NUM_LENGTH) {
+                        sMobileNum = sArrInfo[1];
+                    } else {
+                        sHomeNum = sArrInfo[1];
+                    }
+                    break;
+                default:
+                    //length >= 3
+                    sDisplayName = sArrInfo[0];
+                    sMobileNum = sArrInfo[1];
+                    sHomeNum = sArrInfo[2];
+            }
+            //check sDisplayName sMobileNum and sHomeNum
+            if (sDisplayName == null || sDisplayName.matches(ContactContant.NUM_REGULAR)) {
+                Log.e(m_sTAG, "Error in readArrList sDisplayName == null");
+                m_iFailCount++;
+                continue;
+            }
+            if (sMobileNum != null && (sMobileNum.length() != ContactContant.MOBILE_NUM_LENGTH
+                    || !sMobileNum.matches(ContactContant.NUM_REGULAR))) {
+                Log.e(m_sTAG, "Error in readArrList sMobileNum is not all num or sMobileNum == null");
+                m_iFailCount++;
+                continue;
+            }
+            if (sHomeNum != null && !(sHomeNum.matches(ContactContant.HOME_REGULAR_01) ||
+                    sHomeNum.matches(ContactContant.HOME_REGULAR_02))) {
+                Log.e(m_sTAG, "Error in readArrList sHomeNum is not all num");
+                m_iFailCount++;
+                continue;
+            }
+            arrListContactInfo.add(new ContactInfo(sDisplayName, sMobileNum, sHomeNum));
+        }
+        return arrListContactInfo;
+    }
 
-	public static int getFailCount() {
-		return failCount;
-	}
+    /**
+     * insert into database
+     */
+    private static boolean doInsertToContact(Context context, ContactInfo contactInfo) {
+        Log.d(m_sTAG, "in doInsertToContact contactInfo = null? " + (contactInfo == null));
+        try {
+            ContentValues contentValues = new ContentValues();
+            Uri uri = context.getContentResolver().insert(RawContacts.CONTENT_URI, contentValues);
+            long rowId = ContentUris.parseId(uri);
+
+            String sName = contactInfo.getDisplayName();
+            String sMobileNum = contactInfo.getMobileNum();
+            String sHomeNum = contactInfo.getHomeNum();
+
+            //insert sName
+            if (sName != null) {
+                contentValues.clear();
+                contentValues.put(Data.RAW_CONTACT_ID, rowId);
+                contentValues.put(Data.MIMETYPE, StructuredName.CONTENT_ITEM_TYPE);
+                int iIndex = sName.length() / 2;
+                String sDisplayName = sName;
+                //check sName if english sName
+                String sGivenName = null;
+                String sFamilyName = null;
+                if (checkEnglishName(sDisplayName) == false) {
+                    sGivenName = sName.substring(iIndex);
+                    sFamilyName = sName.substring(0, iIndex);
+                } else {
+                    sGivenName = sFamilyName = sDisplayName;
+                }
+                contentValues.put(StructuredName.DISPLAY_NAME, sDisplayName);
+                contentValues.put(StructuredName.GIVEN_NAME, sGivenName);
+                contentValues.put(StructuredName.FAMILY_NAME, sFamilyName);
+                context.getContentResolver().insert(ContactsContract.Data.CONTENT_URI, contentValues);
+            }
+
+            if (sMobileNum != null) {
+                //insert phone
+                contentValues.clear();
+                contentValues.put(Data.RAW_CONTACT_ID, rowId);
+                contentValues.put(Data.MIMETYPE, Phone.CONTENT_ITEM_TYPE);
+                contentValues.put(Phone.NUMBER, sMobileNum);
+                contentValues.put(Phone.TYPE, Phone.TYPE_MOBILE);
+                context.getContentResolver().insert(ContactsContract.Data.CONTENT_URI, contentValues);
+            }
+
+            if (sHomeNum != null) {
+                //insert houseNum
+                contentValues.clear();
+                contentValues.put(Data.RAW_CONTACT_ID, rowId);
+                contentValues.put(Data.MIMETYPE, Phone.CONTENT_ITEM_TYPE);
+                contentValues.put(Phone.NUMBER, sHomeNum);
+                contentValues.put(Phone.TYPE, Phone.TYPE_HOME);
+                context.getContentResolver().insert(ContactsContract.Data.CONTENT_URI, contentValues);
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean checkEnglishName(String sName) {
+        char[] charsName = sName.toCharArray();
+        for (int i = 0; i < charsName.length; i++) {
+            if ((charsName[i] >= 'a' && charsName[i] <= 'z') ||
+                    (charsName[i] >= 'A' && charsName[i] <= 'Z')) {
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
+
+    public static int getSuccessCount() {
+        return m_iSuccessCount;
+    }
+
+    public static int getFailCount() {
+        return m_iFailCount;
+    }
 }
